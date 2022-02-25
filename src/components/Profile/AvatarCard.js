a26199e2358820b189f968c3e6fd5f5f3e657451
Tejada-Joyce <joyce_105_3@hotmail.com>
2022-02-25 02:02:23 -0700

    created form and added some basic logic for submission

---
+++ src/components/Profile/AvatarCard.js
@@ -0,0 +1,35 @@
+import { useRadio, Box, Avatar } from "@chakra-ui/react";
+
+const AvatarCard = (props) => {
+  const { getInputProps, getCheckboxProps } = useRadio(props);
+
+  const input = getInputProps();
+  const checkbox = getCheckboxProps();
+
+  return (
+    <Box as="label">
+      <input {...input} />
+      <Box
+        {...checkbox}
+        cursor="pointer"
+        borderWidth="1px"
+        borderRadius="md"
+        boxShadow="md"
+        _checked={{
+          bg: "teal.600",
+          color: "white",
+          borderColor: "teal.600",
+        }}
+        _focus={{
+          boxShadow: "outline",
+        }}
+        px={5}
+        py={3}
+      >
+        <Avatar size={["md", "lg"]} name="Username" src={props.children} />
+      </Box>
+    </Box>
+  );
+};
+
+export default AvatarCard;
