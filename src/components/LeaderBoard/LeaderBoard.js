8a2246fd8b3c77fab791b3727802f260cb1cfaa1
Tejada-Joyce <joyce_105_3@hotmail.com>
2022-03-02 05:30:28 -0700

    moved stand-alone funtions to a new file

--- src/components/LeaderBoard/LeaderBoard.js
+++ src/components/LeaderBoard/LeaderBoard.js
@@ -1,78 +1,7 @@
 import CardTop from "./CardTop";
 import { Grid, GridItem } from "@chakra-ui/react";
 import { DUMMY_DATA } from "./dummy_data";
-import { format, isThisMonth, isThisWeek } from "date-fns";
-
-const sortDataByPoints = (objs) => {
-  const sortedData = [...objs].sort((a, b) => {
-    if (b.points < a.points) {
-      return -1;
-    } else if (a.points === b.points) {
-      if (a.accuracy < b.accuracy) {
-        return -1;
-      } else {
-        return 1;
-      }
-    } else {
-      return 1;
-    }
-  });
-  return [...sortedData];
-};
-
-const filterDataByTime = (objs, time) => {
-  let filteredData = [];
-  const today = new Date();
-  switch (time.toLowerCase()) {
-    case "day":
-      filteredData = [...objs].map((obj) => {
-        const questions = obj.questions.filter(
-          (question) => format(today, "MM-dd-yyyy") === question.timestamp
-        );
-        return { ...obj, questions: questions };
-      });
-      filteredData = filteredData.filter((obj) => obj.questions.length !== 0);
-      break;
-    case "week":
-      filteredData = [...objs].map((obj) => {
-        const questions = obj.questions.filter((question) =>
-          isThisWeek(new Date(question.timestamp))
-        );
-        return { ...obj, questions: questions };
-      });
-      filteredData = filteredData.filter((obj) => obj.questions.length !== 0);
-      break;
-    case "month":
-      filteredData = [...objs].map((obj) => {
-        const questions = obj.questions.filter((question) =>
-          isThisMonth(new Date(question.timestamp))
-        );
-        return { ...obj, questions: questions };
-      });
-      filteredData = filteredData.filter((obj) => obj.questions.length !== 0);
-      break;
-    default:
-      break;
-  }
-  return [...filteredData];
-};
-
-const formatUserData = (user) => {
-  const questionsTaken = user.questions.length;
-  let points = 0;
-  user.questions.forEach((question) => {
-    if (question.isCorrect) points++;
-  });
-  const accuracy = ((points * 100) / questionsTaken).toFixed(1);
-  return {
-    id: user.id,
-    username: user.username,
-    avatar: user.avatar,
-    accuracy: `${accuracy}%`,
-    questionsTaken: user.questions.length,
-    points: points,
-  };
-};
+import { sortDataByPoints, filterDataByTime, formatUserData } from "./utils";
 
 const LeaderBoard = () => {
   const sortedUsers = ["Day", "Week", "Month"].map((time) => {
