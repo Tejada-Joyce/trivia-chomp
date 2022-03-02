962a38341f8056885f7447cffc0cc6bb86083158
Tejada-Joyce <joyce_105_3@hotmail.com>
2022-03-02 00:33:30 -0700

    added dummy data and base logic for filtering it

--- src/components/LeaderBoard/LeaderBoard.js
+++ src/components/LeaderBoard/LeaderBoard.js
@@ -1,38 +1,95 @@
 import CardTop from "./CardTop";
 import { Grid, GridItem } from "@chakra-ui/react";
+import { DUMMY_DATA } from "./dummy_data";
+import moment from "moment";
 
-const DUMMY_DATA = [
-  {
-    id: "1",
-    username: "Mary",
-    accuracy: "67%",
-    questionsTaken: "87",
-    avatar: "dinosaur_1",
-  },
-  {
-    id: "2",
-    username: "Mary1",
-    accuracy: "67%",
-    questionsTaken: "87",
-    avatar: "dinosaur_2",
-  },
-  {
-    id: "3",
-    username: "Mary2",
-    accuracy: "67%",
-    questionsTaken: "87",
-    avatar: "dinosaur_3",
-  },
-  {
-    id: "4",
-    username: "Mary3",
-    accuracy: "67%",
-    questionsTaken: "87",
-    avatar: "dinosaur_4",
-  },
-];
+const sortDataByPoints = (objs) => {
+  const sortedData = [...objs].sort((a, b) => {
+    if (b.points < a.points) {
+      return -1;
+    } else if (a.points === b.points) {
+      if (a.accuracy < b.accuracy) {
+        return -1;
+      } else {
+        return 1;
+      }
+    } else {
+      return 1;
+    }
+  });
+  return [...sortedData];
+};
+
+const filterDataByTime = (objs, time) => {
+  let filteredData = [];
+  const today = moment();
+  const currentMonth = today.month();
+  switch (time) {
+    case "day":
+      filteredData = [...objs].map((obj) => {
+        const questions = obj.questions.filter(
+          (question) => today.format("MM-DD-YYYY") === question.timestamp
+        );
+        return { ...obj, questions: questions };
+      });
+      filteredData = filteredData.filter((obj) => obj.questions.length !== 0);
+      break;
+    case "week":
+      filteredData = [...objs].map((obj) => {
+        const questions = obj.questions.filter((question) => {
+          const questionMonth = moment(question.timestamp).month();
+          console.log("month: ", currentMonth);
+          console.log("questionMonth: ", questionMonth);
+          return currentMonth === questionMonth;
+        });
+        return { ...obj, questions: questions };
+      });
+      console.log("filtered after map: ", filteredData);
+      filteredData = filteredData.filter((obj) => obj.questions.length !== 0);
+      console.log("filtered after filter: ", filteredData);
+      break;
+    case "month":
+      filteredData = [...objs].map((obj) => {
+        const questions = obj.questions.filter((question) => {
+          const questionMonth = moment(question.timestamp).month();
+          return currentMonth === questionMonth;
+        });
+        return { ...obj, questions: questions };
+      });
+      filteredData = filteredData.filter((obj) => obj.questions.length !== 0);
+      break;
+    default:
+      break;
+  }
+  return [...filteredData];
+};
+
+const getUserData = (user) => {
+  const questionsTaken = user.questions.length;
+  let points = 0;
+  user.questions.forEach((question) => {
+    if (question.isCorrect) points++;
+  });
+  const accuracy = ((points * 100) / questionsTaken).toFixed(1);
+  return {
+    id: user.id,
+    username: user.username,
+    avatar: user.avatar,
+    accuracy: `${accuracy}%`,
+    questionsTaken: user.questions.length,
+    points: points,
+  };
+};
 
 const LeaderBoard = () => {
+  const users = DUMMY_DATA.map(getUserData);
+  const usersDay = sortDataByPoints(
+    filterDataByTime(DUMMY_DATA, "day").map(getUserData)
+  );
+  const usersMonth = sortDataByPoints(
+    filterDataByTime(DUMMY_DATA, "month").map(getUserData)
+  );
+
   return (
     <Grid
       templateColumns={["1fr", "1fr", "repeat(3, 1fr)"]}
@@ -41,13 +98,13 @@ const LeaderBoard = () => {
       gap={3.5}
     >
       <GridItem bg="rgba(0, 0, 0, .75)" p="20px">
-        <CardTop criteria="Week" users={DUMMY_DATA} />
+        <CardTop time="Day" users={usersDay} />
       </GridItem>
       <GridItem bg="rgba(0, 0, 0, .75)" p="20px">
-        <CardTop criteria="Week" users={DUMMY_DATA} />
+        <CardTop time="Week" users={users} />
       </GridItem>
       <GridItem bg="rgba(0, 0, 0, .75)" p="20px">
-        <CardTop criteria="Week" users={DUMMY_DATA} />
+        <CardTop time="Month" users={usersMonth} />
       </GridItem>
     </Grid>
   );
