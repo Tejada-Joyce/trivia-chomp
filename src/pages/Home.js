39856766095e0d66537dbaf4674c98d94059fed2
Tejada-Joyce <joyce_105_3@hotmail.com>
2022-03-02 14:36:37 -0700

    merged home changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
 import { Heading, Text } from "@chakra-ui/react";
-=======
 import LeaderBoard from "../components/LeaderBoard/LeaderBoard";
->>>>>>> 08eb8922e3a462d42aac7a765f93f83b55505def
 import ProfileCardModal from "../components/Profile/ProfileCardModal";
 import Background from "../components/ui/Background";
 
@@ -11,13 +8,13 @@ const Home = () => {
     <>
       <ProfileCardModal />
       <Background>
-<<<<<<< HEAD
-      <Heading as="h1" textAlign="center">Welcome to TriviaChomp!</Heading>
-      <Text textAlign="center" mt="10px">Today is a good day to do some trivia.</Text>
+        <Heading as="h1" textAlign="center">
+          Welcome to TriviaChomp!
+        </Heading>
+        <Text textAlign="center" mt="10px">
+          Today is a good day to do some trivia.
+        </Text>
         <ProfileCardModal />
-=======
-        <p>Home Page</p>
->>>>>>> 08eb8922e3a462d42aac7a765f93f83b55505def
       </Background>
       <LeaderBoard />
       <div>
