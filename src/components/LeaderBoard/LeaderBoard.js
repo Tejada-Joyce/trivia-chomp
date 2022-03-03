52ead7fd2b81be6cc62482e7f5473dd227999c6e
Tejada-Joyce <joyce_105_3@hotmail.com>
2022-03-02 19:47:29 -0700

    fetched users from database and formattes questions

--- src/components/LeaderBoard/LeaderBoard.js
+++ src/components/LeaderBoard/LeaderBoard.js
@@ -1,14 +1,31 @@
-import CardTop from "./CardTop";
+import { useEffect, useState } from "react";
 import { Grid, GridItem } from "@chakra-ui/react";
-import { DUMMY_DATA } from "../../dummy_data";
+import CardTop from "./CardTop";
 import { sortDataByPoints, filterDataByTime, formatUserData } from "./utils";
+import useHttp from "../../hooks/use-http";
 
 const LeaderBoard = () => {
+  const usersDataUrl =
+    "https://trivia-chomp-c5a02-default-rtdb.firebaseio.com/users.json";
+  const { isLoading, error, sendRequest: getAllUsers } = useHttp();
+  const [usersData, setUsersData] = useState([]);
+
+  useEffect(() => {
+    const formatData = (usersObj) => {
+      const usersArray = Object.entries(usersObj).map(([key, value]) => {
+        return { id: key, ...value };
+      });
+      setUsersData(usersArray);
+    };
+
+    getAllUsers({ url: usersDataUrl }, formatData);
+  }, [getAllUsers, usersDataUrl]);
+
   const sortedUsers = ["Day", "Week", "Month"].map((time) => {
     return {
       time: time,
       users: sortDataByPoints(
-        filterDataByTime(DUMMY_DATA, time).map(formatUserData)
+        filterDataByTime(usersData, time).map(formatUserData)
       ),
     };
   });
