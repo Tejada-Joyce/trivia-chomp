5dd935c19d9eff6e943cbdbb0a0e023bb74cd01f
Cierra Morris <4cibe@MacBook-Pro-10.localdomain>
2022-03-02 17:06:28 -0700

    added quiz stuff

--- src/pages/Quiz.js
+++ src/pages/Quiz.js
@@ -4,9 +4,11 @@ import Background from "../components/ui/Background";
 import { useParams } from "react-router";
 import useHttp from "../hooks/use-http";
 import { useState, useEffect } from "react";
-import { Button, Progress, Radio, RadioGroup, Spinner, Stack, Alert, AlertDescription, ListItem, List, ListIcon } from "@chakra-ui/react";
+import { Button, Progress, Radio, RadioGroup, Spinner, Stack, Alert, AlertDescription, ListItem, List, ListIcon, Heading, Text, Box } from "@chakra-ui/react";
 import React from "react";
 import { CheckCircleIcon, SmallCloseIcon } from "@chakra-ui/icons";
+import { format } from "date-fns";
+import { useNavigate } from "react-router";
 
 const decodeHTML = function (html) {
   var txt = document.createElement('textarea');
@@ -17,6 +19,7 @@ const decodeHTML = function (html) {
 const Quiz = () => {
   const { isLoading, error, sendRequest } = useHttp()
   const { categoryId } = useParams();
+  const navigate = useNavigate();
 
   //questions from API
   const [questions, setQuestions] = useState([]);
@@ -31,7 +34,7 @@ const Quiz = () => {
   //the index of the choice the user picked
   const [userChoice, setUserChoice] = useState(null)
   //whether or not the user answered the current question correctly
-  const [isCorrect, setIsCorrect] = useState(false)
+  const [isCorrect, setIsCorrect] = useState(0)
   //whether or not the user answered
   const [answered, setAnswered] = useState(false)
   //an array of info about the questions as will be stored in our database : 
@@ -41,6 +44,7 @@ const Quiz = () => {
   //   isCorrect: Boolean
   // }
   const [questionData, setQuestionData] = useState([])
+  const [finished, setFinished] = useState(false)
 
   let category = ''
   if (categoryId !== 'random') {
@@ -52,6 +56,16 @@ const Quiz = () => {
     setValue(prev => {
       return e.toString()
     })
+  }
+
+  const playAgainHandler = () => {
+    console.log('going agin')
+    setQuestions([]);
+    setCurQuestion(0);
+    setQuestionData([]);
+    setFinished(false);
+    navigate(`/`, {replace: true})
+    
   }
 
   const questionUrl = `https://opentdb.com/api.php?amount=10&category=${category}`
@@ -104,11 +118,11 @@ const Quiz = () => {
     setUserChoice(userAnswer)
     if (userAnswer === correctAnswer) {
       //give user feedback 
-      setIsCorrect(true)
-      
+      setIsCorrect(1)
+
 
     } else {
-      setIsCorrect(false)
+      setIsCorrect(0)
     }
 
   }
@@ -117,22 +131,33 @@ const Quiz = () => {
     //add to the array to store in database
     const curQuestionData = {
       category: questions[curQuestion]?.category,
-      timestamp: new Date(),
+      timestamp: format(new Date(), 'MM-dd-yyy'),
       isCorrect: isCorrect
     }
-    
+
     setQuestionData(prev => {
       return [...prev, curQuestionData]
     })
-    console.log(questionData)
 
     if (curQuestion < questions.length - 1) {
       setCurQuestion(prev => prev + 1)
-      setIsCorrect(false);
+      setIsCorrect(0);
       setAnswered(false)
       setValue(0)
     } else {
-      console.log('finished')
+      setFinished(true);
+      //send data and trigger finished screen
+      const userId = '1' //replace with real user id once we have context set up
+      const endpoint = `https://trivia-chomp-c5a02-default-rtdb.firebaseio.com/users/${userId}/questions.json`
+      questionData.forEach(answeredQuestion => {
+        sendRequest({
+          url: endpoint,
+          method: 'POST',
+          body: answeredQuestion,
+          headers: { 'Content-Type': 'application/json' }
+        }, (response) => {})
+
+      })
     }
   }
 
@@ -167,17 +192,17 @@ const Quiz = () => {
             {
               answers.map((answer, index) => {
                 if (correctAnswer == index) {
-                  return <ListItem key={index}>
-                    <ListIcon as={CheckCircleIcon} color="green"/>
-                    {answer}
+                  return <ListItem key={index} bg={userChoice == index && 'rgba(255,249,196, .5)'}>
+                    <ListIcon as={CheckCircleIcon} color="green" />
+                    {decodeHTML(answer)}
                   </ListItem>
                 } else {
-                  return <ListItem key={index}>
-                    <ListIcon  as={SmallCloseIcon} color="red"/>
-                    {answer}
+                  return <ListItem key={index} bg={userChoice == index && 'rgba(255,249,196, .5)'}>
+                    <ListIcon as={SmallCloseIcon} color="red" />
+                    {decodeHTML(answer)}
                   </ListItem>
                 }
-                })
+              })
             }
           </List>
         }
@@ -188,12 +213,25 @@ const Quiz = () => {
     </div>
   );
   if (error) {
-    content = <p>There was a problem</p>
+    content = <div><p>There was a problem</p></div>
     console.log(error)
   }
 
   if (isLoading) {
     content = <Spinner />
+  }
+
+  if (finished) {
+    content = <Box textAlign="center">
+      <Heading as="h1">QUIZ COMPLETE</Heading>
+      <Text fontSize="4xl">{questionData.reduce((total, curr) => {
+        if (curr.isCorrect) {
+          return total + 1
+        } 
+        return total
+      }, 0)} / {questions.length}</Text>
+      <Button onClick={playAgainHandler}>Finished</Button>
+    </Box>
   }
 
   return (
