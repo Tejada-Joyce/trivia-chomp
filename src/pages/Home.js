814ff9d843ba936ee4ddecccf32d08de9c9354ff
Tejada-Joyce <joyce_105_3@hotmail.com>
2022-03-09 14:55:00 -0700

    cleaned code to avoid console errors

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,15 +10,16 @@ import AuthContext from "../store/auth-contex";
 
 const Home = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const { isLoading, error, sendRequest: getUserData } = useHttp();
+  const { sendRequest: getUserData } = useHttp();
   const [showModal, setShowModal] = useState(false);
   const authCtx = useContext(AuthContext);
+  const { updateUserData } = authCtx;
   const userId = authCtx.userId;
 
   useEffect(() => {
     //get other user data
     const callback = (data) => {
-      authCtx.updateUserData(data);
+      updateUserData(data);
       setShowModal(true);
     };
     getUserData(
@@ -27,30 +28,26 @@ const Home = () => {
       },
       callback
     );
-  }, [userId, getUserData]);
+  }, [userId, getUserData, updateUserData]);
 
   return (
-    <>
-      {!isLoading && !error && (
-        <Flex flexDir="column" justify="space-between" h="100%">
-          <div>
-            <Background>
-              <Heading as="h1" textAlign="center">
-                Welcome to TriviaChomp, {authCtx.username}!
-              </Heading>
-              <Text textAlign="center" mt="10px">
-                Today is a good day to do some trivia.
-              </Text>
-              <QuizSetupModal isOpen={isOpen} onClose={onClose} />
-              <QuizStartButton onClick={onOpen} />
-              {showModal && <ProfileCardModal />}
-            </Background>
-            <Divider height="5px" borderColor="transparent" />
-            <LeaderBoard />
-          </div>
-        </Flex>
-      )}
-    </>
+    <Flex flexDir="column" justify="space-between" h="100%">
+      <div>
+        <Background>
+          <Heading as="h1" textAlign="center">
+            Welcome to TriviaChomp, {authCtx.username}!
+          </Heading>
+          <Text textAlign="center" mt="10px">
+            Today is a good day to do some trivia.
+          </Text>
+          <QuizSetupModal isOpen={isOpen} onClose={onClose} />
+          <QuizStartButton onClick={onOpen} />
+          {showModal && <ProfileCardModal />}
+        </Background>
+        <Divider height="5px" borderColor="transparent" />
+        <LeaderBoard />
+      </div>
+    </Flex>
   );
 };
 
