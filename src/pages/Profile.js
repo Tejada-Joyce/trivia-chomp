123fa53a5af75de680b141e97a8606726a44bb53
Tejada-Joyce <joyce_105_3@hotmail.com>
2022-03-08 20:38:44 -0700

    eslint - prettier changes

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,68 +1,84 @@
 import Background from "../components/ui/Background";
 import AuthContext from "../store/auth-contex";
 import { useContext, useEffect, useState } from "react";
-import { Avatar, Center, Divider, Heading, Stack, Stat, StatArrow, StatGroup, StatHelpText, StatLabel, StatNumber } from "@chakra-ui/react";
+import {
+  Avatar,
+  Center,
+  Divider,
+  Heading,
+  Stat,
+  StatArrow,
+  StatGroup,
+  StatHelpText,
+  StatLabel,
+  StatNumber,
+} from "@chakra-ui/react";
 import useHttp from "../hooks/use-http";
 import dinosaurs from "../images/index";
-import { matchRoutes } from "react-router";
-
 
 const Profile = () => {
-  const authCtx = useContext(AuthContext)
+  const authCtx = useContext(AuthContext);
   const { isLoading, error, sendRequest } = useHttp();
-  const [questions, setQuestions] = useState({});
-  const [avatar, setAvatar] = useState('')
-  const [username, setUsername] = useState('')
-  const [totalQuestionsAnswered, setTotalQuestionsAnswered] = useState(0)
-  const [correctAnswered, setCorrectAnswered] = useState(0)
-  const [incorrectAnswered, setIncorrectAnswered] = useState(0)
+  const [avatar, setAvatar] = useState("");
+  const [username, setUsername] = useState("");
+  const [totalQuestionsAnswered, setTotalQuestionsAnswered] = useState(0);
+  const [correctAnswered, setCorrectAnswered] = useState(0);
+  const [incorrectAnswered, setIncorrectAnswered] = useState(0);
 
   useEffect(() => {
-    const userId = authCtx.userId
-    sendRequest({url: `https://trivia-chomp-c5a02-default-rtdb.firebaseio.com/users/${userId}.json`}, (data) => {
-      data?.avatar && setAvatar(data.avatar)
-      data?.username && setUsername(data.username)
+    const userId = authCtx.userId;
+    sendRequest(
+      {
+        url: `https://trivia-chomp-c5a02-default-rtdb.firebaseio.com/users/${userId}.json`,
+      },
+      (data) => {
+        data?.avatar && setAvatar(data.avatar);
+        data?.username && setUsername(data.username);
 
-      if (data?.questions) {
-        for (const question in data.questions) {
-          setTotalQuestionsAnswered(prev => prev + 1)
-          if (data.questions[question].isCorrect) {
-            setCorrectAnswered(prev => prev + 1)
-          } else {
-            setIncorrectAnswered(prev => prev + 1)
+        if (data?.questions) {
+          for (const question in data.questions) {
+            setTotalQuestionsAnswered((prev) => prev + 1);
+            if (data.questions[question].isCorrect) {
+              setCorrectAnswered((prev) => prev + 1);
+            } else {
+              setIncorrectAnswered((prev) => prev + 1);
+            }
           }
         }
-
       }
-
-  })
-
+    );
   }, [authCtx.userId, sendRequest]);
   const avatarNum = avatar.slice(9);
-  const image = dinosaurs[+avatarNum - 1]; 
+  const image = dinosaurs[+avatarNum - 1];
 
   return (
     <Background>
       <Center>
-      <Avatar margin="auto" size="xl" src={image}></Avatar>
+        <Avatar margin="auto" size="xl" src={image}></Avatar>
       </Center>
-      <Heading as="h2" size="lg" textAlign="center">{username}</Heading>
-      <Heading as="h3" size="md">Stats</Heading>
+      <Heading as="h2" size="lg" textAlign="center">
+        {username}
+      </Heading>
+      <Heading as="h3" size="md">
+        Stats
+      </Heading>
       <Divider m="5px 0"></Divider>
       <StatGroup>
-      <Stat>
+        <Stat>
           <StatLabel>Total Questions Answered</StatLabel>
           <StatNumber>{totalQuestionsAnswered}</StatNumber>
-          <StatHelpText>
-
-          </StatHelpText>
+          <StatHelpText></StatHelpText>
         </Stat>
         <Stat>
           <StatLabel>Correct</StatLabel>
           <StatNumber>{correctAnswered}</StatNumber>
           <StatHelpText>
             <StatArrow type="increase"></StatArrow>
-            {(totalQuestionsAnswered > 0) ? Math.round(correctAnswered * 1000 / totalQuestionsAnswered) / 100 :'--' }%
+            {totalQuestionsAnswered > 0
+              ? Math.round((correctAnswered * 1000) / totalQuestionsAnswered) /
+                100
+              : "--"}
+            %
           </StatHelpText>
         </Stat>
         <Stat>
@@ -70,9 +86,14 @@ const Profile = () => {
           <StatNumber>{incorrectAnswered}</StatNumber>
           <StatHelpText>
             <StatArrow type="decrease"></StatArrow>
-            {(totalQuestionsAnswered > 0) ? Math.round(incorrectAnswered * 1000 / totalQuestionsAnswered)/100 :'--' }%
+            {totalQuestionsAnswered > 0
+              ? Math.round(
+                  (incorrectAnswered * 1000) / totalQuestionsAnswered
+                ) / 100
+              : "--"}
+            %
           </StatHelpText>
-          </Stat>
+        </Stat>
       </StatGroup>
     </Background>
   );
